459be5b52ee2854196015350c9373c547bcfdfb4
BurlyOctopus <joseph.clarkson20@gmail.com>
2016-07-28 21:08:59 -0400

    Adding structure
    Added needed files and started to create demos.

--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,3 @@
-# Click-Events
-Some Demo's that show different ways to bind click events
-
-
-```html
-<ol id="orderedList">
-  <li><a href="">Link 1</a></li>
-  <li><a href="">Link 2</a></li>
-  <li><a href="">Link 3</a></li>
-  <li><a href="">Link 4</a></li>
-  <li><a href="">Link 5</a></li>
-  <li><a href="">Link 6</a></li>
-</ol>
-```
-
-```javascript
 // locate ordered list and add the Click Event Listener
 document.getElementById("orderedList").addEventListener("click",function(e) {
     // e.target is our targetted element, aka a tag.
@@ -22,4 +6,3 @@ document.getElementById("orderedList").addEventListener("click",function(e) {
         console.log(e.target.innerHTML + " was clicked");
     }
 });
-```
